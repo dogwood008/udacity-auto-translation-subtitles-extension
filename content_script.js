b9cf2e16a483cf8f4f0ee9cfecb6b55b0e153af3
dogwood008 <dogwood008+github@gmail.com>
2017-04-06 00:06:31 +0900

    :+1: fix script to translate from prepared English to Japanese

--- content_script.js
+++ content_script.js
@@ -1,19 +1,30 @@
 (function() {
     (function($) {
-        var delay = 5000;
+        var delay = 3000;
         console.log(delay / 1000 + '秒後に日本語字幕（翻訳）へ切り替えます');
+        console.log($);
         setTimeout(function() {
             var delayInMenuSelect = 100;
             $('.ytp-settings-button').click();
             setTimeout(function() {
-                $('div.ytp-popup.ytp-settings-menu > div > div > div:nth-child(2)').click();
+                $('.ytp-menuitem:contains("字幕")').click();
             }, delayInMenuSelect * 1);
             setTimeout(function() {
-                $('div.ytp-popup.ytp-settings-menu > div > div.ytp-panel-menu > div:last-child').click();
+                $($.map($('.ytp-menuitem:contains("英語")'), function(val) {
+                    return $(val).text().match(/自動生成/) ? null : val;
+                })[0]).click();
             }, delayInMenuSelect * 2);
             setTimeout(function() {
-                $('div.ytp-popup.ytp-settings-menu > div > div.ytp-panel-menu > div:last-child').click();
-            }, delayInMenuSelect * 3);
+                $('.ytp-menuitem:contains("字幕")').click();
+            }, delayInMenuSelect * 9);
+            setTimeout(function() {
+                $('.ytp-menuitem:contains("自動翻訳")').click();
+            }, delayInMenuSelect * 9);
+            setTimeout(function() {
+                $('.ytp-menuitem:contains("日本語")').click();
+		$('.ytp-settings-button').click();
+            }, delayInMenuSelect * 9);
         }, delay);
     })(jQuery)
+
 }());
